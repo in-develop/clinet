35ae323a930bbb0678f0304601297d7d77565c36
Poltorak Denys <itwfrontend@gmail.com>
2025-08-10 11:40:16 +0300

    feat: add prettier plugin to format tailwind classes

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col row-start-2">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
+      <main className="row-start-2 flex flex-col">
         <h1>Hello team!</h1>
       </main>
     </div>
