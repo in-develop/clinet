70d5ab2c77b891737ea65f05082acb72c8343cb6
Poltorak Denys <itwfrontend@gmail.com>
2025-08-21 18:08:16 +0300

    feat: add header destop/mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,22 @@
 import StoreProvider from "@/app/providers/StoreProvider";
 import { syne } from "@/shared/lib/fonts";
 import "@/app/globals.css";
+import { Header } from "@/widgets";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user = {};
+
   return (
     <html lang="en">
       <body className={syne.className}>
-        <StoreProvider>{children}</StoreProvider>
+        <StoreProvider>
+          <Header isAuthenticated={!!user} />
+          {children}
+          </StoreProvider>
       </body>
     </html>
   );
