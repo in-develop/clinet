393cabdd0859f2e120c45315813f832289dda607
Oleksandr Panichuk <oleksandr.panichuk.dev@gmail.com>
2025-08-31 07:28:11 +0300

    feat: add links to footer

--- src/widgets/footer/model/constants.ts
+++ src/widgets/footer/model/constants.ts
@@ -1,5 +1,7 @@
 import { TFooterLink } from "./types";
 
+import { ROUTING } from "@/shared/lib/rounting";
+
 export const shopLinks: TFooterLink[] = [
   { label: "Le Pelerin Perfume", href: "#" },
   { label: "Piel Cosmetics Line", href: "#" },
@@ -14,21 +16,21 @@ export const shopLinks: TFooterLink[] = [
 ];
 
 export const pagesLinks: TFooterLink[] = [
-  { label: "Cosmetologist", href: "#" },
-  { label: "About us", href: "#" },
-  { label: "Cooperation", href: "#" },
-  { label: "Delivery & Payments", href: "#" },
-  { label: "FAQ", href: "#" },
-  { label: "Blog", href: "#" },
-  { label: "Account", href: "#" },
-  { label: "Contact", href: "#" },
+  { label: "Cosmetologist", href: ROUTING.cosmetologist },
+  { label: "About us", href: ROUTING.aboutUs },
+  { label: "Cooperation", href: ROUTING.cooperation },
+  { label: "Delivery & Payments", href: ROUTING.deliveryPayments },
+  { label: "FAQ", href: ROUTING.faq },
+  { label: "Blog", href: ROUTING.blog },
+  { label: "Account", href: ROUTING.account },
+  { label: "Contact", href: ROUTING.contact },
 ];
 
 export const infoLinks: TFooterLink[] = [
-  { label: "Privacy policy", href: "#" },
-  { label: "Returns and refunds", href: "#" },
-  { label: "Shipping policy", href: "#" },
-  { label: "Terms & Conditions", href: "#" },
+  { label: "Privacy policy", href: ROUTING.privacy },
+  { label: "Returns and refunds", href: ROUTING.refunds },
+  { label: "Shipping policy", href: ROUTING.shippingPolicy },
+  { label: "Terms & Conditions", href: ROUTING.terms },
 ];
 
 export const socialLinks: TFooterLink[] = [
