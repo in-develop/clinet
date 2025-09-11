30aaaf0059ef5e50cd2f843451326a3d317af3fe
AndreevaYuliya <yuliya2080219@gmail.com>
2025-09-11 22:46:12 +0300

    feat(ui): add get consultation modal

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,9 @@
-"use client";
-
-import { GetConsultationModal } from "@/shared/ui/Modals";
 import { Hero } from "@/widgets/hero";
 
 export default function Home() {
   return (
     <div className="mx-auto pt-[6rem] sm:pt-[7.4375rem]">
       <Hero />
-
-      <GetConsultationModal onClose={() => {}} />
     </div>
   );
 }
