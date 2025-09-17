4d664fb7e56a125b18c54d2adb4406eb92d08cec
AndreevaYuliya <yuliya2080219@gmail.com>
2025-09-17 22:15:08 +0300

    fix: delete imports

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,4 @@
 import { Hero } from "@/widgets/hero";
-import { PromoCard } from "@/widgets/promoSlider";
-import { PromoCarousel } from "@/widgets/promoSlider/ui/PromoCarousel";
 import { PromoSlider } from "@/widgets/promoSlider/ui/PromoSlider";
 
 export default function Home() {
